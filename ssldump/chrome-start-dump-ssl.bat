ad62cf24369dcd7ab24300e46a5dfee49b50ed02
rohess <38165623+rohess@users.noreply.github.com>
2024-10-14 22:15:05 +0200

    change comments to proper REM

--- ssldump/chrome-start-dump-ssl.bat
+++ ssldump/chrome-start-dump-ssl.bat
@@ -1,15 +1,13 @@
-# make sure you can write there
+REM make sure you can write there
 if not exist c:\temp  mkdir c:\temp
 
-# name of file is arbitrary - just make sure its configured 
-# in Wireshark under protocols/TLS as (Pre)-Master-Secret logfile name
+REM name of file is arbitrary - just make sure its configured 
+REM in Wireshark under protocols/TLS as (Pre)-Master-Secret logfile name
 set SSLKEYLOGFILE=C:\temp\SSLKEYFILE
 
-# make sure Chrome is not already running
+REM make sure Chrome is not already running
 Taskkill /F /IM chrome.exe
 
-# change path as needed
-# make sure chrome is not already running, otherwise it reuses the running process,
-# which does not see the environment variable
+REM change path as needed
 "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
