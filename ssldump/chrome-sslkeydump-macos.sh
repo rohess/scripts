45cb96b125fd4452733d76b5612fdda34b27c819
rohess <38165623+rohess@users.noreply.github.com>
2024-10-28 21:16:24 +0100

    add killall command to kill already runnig chrome instances before start

--- ssldump/chrome-sslkeydump-macos.sh
+++ ssldump/chrome-sslkeydump-macos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # we use the home directory - because only this seems realiably writable. 
 export SSLKEYLOGFILE=~/SSLKEYFILE
+# kill running instances, otherwiese the env var is not picked up
+killall -9 "Google Chrome"
 # open chrome with webrtc internals and the page with the goto sessions
 open -a 'google chrome.app' "chrome://webrtc-internals" "https://app.goto.com/meeting/957100141"
