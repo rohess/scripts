eb9677b688772d0d479867c2299ce62ca46af9e1
rohess <38165623+rohess@users.noreply.github.com>
2024-10-28 19:12:27 +0100

    add URLs to open automatically

--- ssldump/chrome-sslkeydump-macos.sh
+++ ssldump/chrome-sslkeydump-macos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # we use the home directory - because only this seems realiably writable. 
 export SSLKEYLOGFILE=~/SSLKEYFILE
-open -a "Google Chrome.app"
+# open chrome with webrtc internals and the page with the goto sessions
+open -a 'google chrome.app' "chrome://webrtc-internals" "https://app.goto.com/meeting/957100141"
