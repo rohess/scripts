815457e2fdedf4a7f524c79b8370d50d61c81234
Robert Hess <robert.hess@logmein.com>
2026-09-18 14:16:44 +0200

    changed comments

--- ssldump/chrome-sslkeydump-win.bat
+++ ssldump/chrome-sslkeydump-win.bat
@@ -14,7 +14,9 @@ start chrome.exe
 REM alternatively you can qualify your full path - but this is system dependend
 REM "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
-REM start capture via dumbcap
+REM start capture via dumpcap 
+REM maybe add pause before - or just fire it ahead 
+dumpcap -i 5 -n -w C:\temp\test.pcapng -b duration:900 -b files:50
 
 REM keep shell open to see what has happened in case of errors 
 pause
