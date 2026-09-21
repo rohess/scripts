a9aba387fd3e61292ea5e4f36f3cabffad3c64c1
Robert Hess <rhess@posteo.de>
2026-09-21 14:03:45 +0200

    use start for dumpcap, as otherwise chrome start is never alled, taskkill to end dumpcap running in extra window

--- ssldump/chrome-sslkeydump-win.bat
+++ ssldump/chrome-sslkeydump-win.bat
@@ -7,8 +7,6 @@ REM TODO: move hardcoded stuff to variables
 REM ###############################
 
 
-
-
 REM Name for capture - could be "teams", "meet", etc
 
 set LABEL=%~1
@@ -33,12 +31,12 @@ REM start capture via dumpcap - filter some noise, keep mdns udp 5353 to see wha
 REM create timestamp - wild PS magic, but "date" doesn't cut it
 for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMdd_HHmmss"') do set TS=%%i
 
-"C:\Program Files\Wireshark\dumpcap.exe"   -i ethernet -n -f "(not broadcast and not multicast and not port 3389) or udp port 5353" -w "c:\temp\cap\%LABEL%_%TS%.pcapng"
+start "dumpcap" "C:\Program Files\Wireshark\dumpcap.exe"   -i ethernet -f "(not broadcast and not multicast and not port 3389) or udp port 5353" -w "c:\temp\cap\%LABEL%_%TS%.pcapng"
 
 REM start searches for chrome.exe, regardless where it is
 REM start chrome.exe
 
-start "" chrome.exe ^
+start ""  chrome.exe ^
   --user-data-dir=C:\demo-profile ^
   --disable-extensions ^
   --use-fake-device-for-media-stream ^
@@ -54,3 +52,5 @@ powershell -NoProfile -Command "Invoke-RestMethod -Method Put -Uri 'http://127.0
 REM keep shell open as long as capture needs to run
 echo "Stop capture"
 pause
+
+taskkill /im dumpcap.exe /f
